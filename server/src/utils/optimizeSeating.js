7ad40c5adac5186e3b0e0e46af6db04fbac69fd7
minhty123 <minhty1456@gmail.com>
2024-05-30 22:53:07 +0700

    update dieu kien + xoa mang

--- server/src/utils/optimizeSeating.js
+++ server/src/utils/optimizeSeating.js
@@ -1,7 +1,4 @@
 const munkresAlgorithm = require('./munkresAL.js');
-const Table = require('../app/models/Table');
-const Customer = require('../app/models/Customer.js');
-const { set } = require('mongoose');
 
 async function optimizeSeating(customers, tables) {
   customers.sort((a, b) => a.checkin - b.checkin);
@@ -32,7 +29,7 @@ async function optimizeSeating(customers, tables) {
       }
     }
   }
-  // console.table(costMatrix);
+  console.table(costMatrix);
   let Arrange;
   for (i = 0; i < tables.length; i++) {
     tables[i].index = i;
@@ -90,124 +87,33 @@ async function optimizeSeating(customers, tables) {
   // Convert the set to an array
   let resArray = Array.from(res);
   let seen = new Set();
-  // for (let i = 0; i < resArray.length; i++) {
-  //   if (!seen.has(resArray[i][0])) {
-  //     seen.add(resArray[i][0]);
-  //     result.add(resArray[i]);
-  //   } else {
-  //     for (let j = i + 1; j < resArray.length; j++) {
-  //       let checkoutPlus15Minutes = addMinutes(
-  //         customers[resArray[i][1]].checkout,
-  //         15
-  //       );
-  //       if (
-  //         checkoutPlus15Minutes >= customers[resArray[j][1]].checkin &&
-  //         customers[resArray[i][1]].catetable ===
-  //           customers[resArray[j][1]].catetable
-  //       ) {
-  //         if (!seen.has(resArray[j][0])) {
-  //           seen.add(resArray[j][0]);
-  //           result.add(resArray[j]);
-  //         } else {
-  //           removed.add(resArray[j]);
-  //         }
-  //       } else {
-  //         result.add(resArray[j]);
-  //       }
-  //     }
-  //   }
-  // }
-
   let addedFirstTime = new Set(); // Tạo một Set để lưu trữ các giá trị đã xuất hiện lần đầu tiên
-  // for (let i = 0; i < resArray.length; i++) {
-  //   if (!addedFirstTime.has(resArray[i][0])) {
-  //     result.add(resArray[i]); // Thêm phần tử resArray[i] vào result
-  //     addedFirstTime.add(resArray[i][0]); // Đánh dấu giá trị tại resArray[i][0] đã xuất hiện lần đầu
-  //   }
-  //   for (let j = i + 1; j < resArray.length; j++) {
-  //     if (resArray[i][0] === resArray[j][0]) {
-  //       let checkoutPlus15Minutes = addMinutes(
-  //         customers[resArray[i][1]].checkout,
-  //         15
-  //       );
-  //       if (checkoutPlus15Minutes >= customers[resArray[j][1]].checkin) {
-  //         result.add(resArray[i]);
-  //         removed.add(resArray[j]);
-  //         resArray.splice(j, 1);
-  //         console.log(resArray);
-  //       } else {
-  //         result.add(resArray[j]);
-  //       }
-  //     }
-  //   }
-  // }
-
   for (let i = 0; i < resArray.length; i++) {
     if (!addedFirstTime.has(resArray[i][0])) {
-      result.add(resArray[i]);
-      addedFirstTime.add(resArray[i][0]);
+      result.add(resArray[i]); // Thêm phần tử resArray[i] vào result
+      addedFirstTime.add(resArray[i][0]); // Đánh dấu giá trị tại resArray[i][0] đã xuất hiện lần đầu
     }
-    let j = i + 1;
-    while (j < resArray.length) {
+    let checkoutPlus15Minutes = addMinutes(
+      customers[resArray[i][1]].checkout,
+      15
+    );
+    for (let j = i + 1; j < resArray.length; j++) {
       if (resArray[i][0] === resArray[j][0]) {
-        let checkoutPlus15Minutes = addMinutes(
-          customers[resArray[i][1]].checkout,
-          15
-        );
         if (checkoutPlus15Minutes >= customers[resArray[j][1]].checkin) {
           result.add(resArray[i]);
           removed.add(resArray[j]);
-          resArray.splice(j, 1);
-          console.log(resArray);
         } else {
           result.add(resArray[j]);
-          j++; // Tăng chỉ số j nếu không xóa phần tử
         }
-      } else {
-        j++; // Tăng chỉ số j nếu không có kết nối
       }
     }
   }
 
-  // for (let i = 0; i < resArray.length; i++) {
-  //   result.add(resArray[0]);
-  //   for (let j = i + 1; j < resArray.length; j++) {
-  //     let checkoutPlus15Minutes = addMinutes(
-  //       customers[resArray[i][1]].checkout,
-  //       15
-  //     );
-  //     if (
-  //       checkoutPlus15Minutes >= customers[resArray[j][1]].checkin &&
-  //       customers[resArray[i][1]].catetable ===
-  //         customers[resArray[j][1]].catetable
-  //     ) {
-  //       result.add(resArray[i]);
-  //       removed.add(resArray[j]);
-  //     } else {
-  //       result.add(resArray[j]);
-  //     }
-  //   }
-  // }
-
-  // for (let i = 0; i < resArray.length; i++) {
-  //   result.add(resArray[0]);
-  //   for (let j = i + 1; j < resArray.length; j++) {
-  //     if (
-  //       customers[resArray[i][1]].checkout >= customers[resArray[j][1]].checkin
-  //     ) {
-  //       result.add(resArray[i]);
-  //       removed.add(resArray[j]);
-  //     } else {
-  //       result.add(resArray[j]);
-  //     }
-  //   }
-  // }
   // removed.forEach((item) => {
   //   if (result.has(item)) {
   //     result.delete(item);
   //   }
   // });
-
   console.log(res);
   console.log(result);
   console.log(removed);
@@ -219,7 +125,6 @@ async function optimizeSeating(customers, tables) {
   //   } else {
   //     const prevIndex = result.findIndex((item) => item[0] === firstElement);
   //     const prevSublist = result[prevIndex];
-
   //     if (customers[prevSublist[1]].checkout <= customers[sublist[1]].checkin) {
   //       removed.push(sublist);
   //     } else {
@@ -228,7 +133,6 @@ async function optimizeSeating(customers, tables) {
   //     }
   //   }
   // });
-
   let removedArr = Array.from(removed);
   for (i = 0; i < removedArr.length; i++) {
     tables[removedArr[i][0]].status = 'Đã gán';
@@ -260,5 +164,4 @@ async function optimizeSeating(customers, tables) {
 
   return customers;
 }
-
-module.exports = optimizeSeating;
+exports.optimizeSeating = optimizeSeating;
